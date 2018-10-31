82b8c4e547b43794bfed9d9d41dcfdd612a5aae4
ducnguyen2104 <ducnguyen2104@gmail.com>
2018-10-31 10:02:41 +0700

    Initial Commit

---
+++ CustomUILabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomUILabel
+//
+//  Created by CPU11613 on 10/31/18.
+//  Copyright © 2018 CPU11613. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
